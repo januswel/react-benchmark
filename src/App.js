5e504cfdb286c008b13fa5e8d8ce597b5c783740
Takagi Kensuke <janus.wel.3@gmail.com>
2021-05-27 02:09:13 +0900

    :sparkles: implement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Benchmark, {BenchmarkType} from 'react-component-benchmark'
+
+function Div() {
+  return (
+    <div>
+      <p>div component</p>
+      <p>div component</p>
+    </div>
+  );
+}
+
+function Flagment() {
+  return (
+    <>
+      <p>div component</p>
+      <p>div component</p>
+    </>
+  );
+}
 
 function App() {
+  const flagmentRef = React.useRef();
+  const divRef = React.useRef();
+  const [flagmentResult, setFlagmentResult] = React.useState()
+  const [divResult, setDivResult] = React.useState()
+
+  const handleCompleteFlagment = React.useCallback((results) => {
+    setFlagmentResult({...results, component: 'Flagment'})
+    console.dir(results)
+  }, []);
+  const handleCompleteDiv = React.useCallback((results) => {
+    setDivResult({...results, component: 'Div'})
+    console.dir(results)
+  }, []);
+
+  function handleStart() {
+    flagmentRef.current.start();
+    divRef.current.start();
+  }
+
+  const columns = ["component", "sampleCount", "max", "min", "mean", "median", "p70", "p95", "p99"]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleStart}>Run</button>
+      <Benchmark
+        component={Div}
+        onComplete={handleCompleteDiv}
+        ref={divRef}
+        samples={100}
+        timeout={10000}
+        type={BenchmarkType.MOUNT}
+      />
+      <Benchmark
+        component={Flagment}
+        onComplete={handleCompleteFlagment}
+        ref={flagmentRef}
+        samples={100}
+        timeout={10000}
+        type={BenchmarkType.MOUNT}
+      />
+      {divResult && flagmentResult &&
+      <table>
+        <thead>
+          <tr>
+          {columns.map(title => <td>{title}</td>)}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {columns.map(property => <td>{divResult[property]}</td>)}
+          </tr>
+          <tr>
+            {columns.map(property => <td>{flagmentResult[property]}</td>)}
+          </tr>
+        </tbody>
+      </table>
+      }
     </div>
   );
 }
